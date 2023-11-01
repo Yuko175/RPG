2b88e73a6893eb9a25c15072200e6c52d58f986e
Yuko175 <happyyuko175@gmail.com>
2023-11-01 23:58:05 +0900

    修正：RPG(関数に分ける)

--- Character.py
+++ Character.py
@@ -2,11 +2,21 @@ class Character:
     def __init__(self, name=None):
         self.HP = 100
         self.ATK = 10
+        self.HEAL = 20
         self.name = name
 
     def attack(self, object):
         if self.HP > 0:
             object.HP -= self.ATK
+
+    def heal(self, object):
+        if self.HP < 100:
+            object.HP += self.HEAL
+        else:
+            print("HPが満タンです")
+        if self.HP >= 100:
+            object.HP = 100
+            print("HPが満タンです")
 
 
 class Player(Character):
--- RPG.py
+++ RPG.py
@@ -8,28 +8,50 @@ import random
 
 def main():
     character_list: list = []
-    player = Player("Alice")
-    player2 = Player("Bob")
+    main_player_name = input("名前を入力してください：")
+    set_main_player_name(character_list, main_player_name)
+    set_sub_player_name(character_list)
+    set_enemy_name(character_list)
+
+    print(battle(character_list, main_player_name))
+
+
+def set_main_player_name(character_list, main_player_name):
+    player = Player(main_player_name)
     character_list.append(player)
+
+
+def set_sub_player_name(character_list):
+    player2 = Player("Bob")
     character_list.append(player2)
 
+
+def set_enemy_name(character_list):
     for i in range(3):
         enemy = Enemy(f"Slime_{i}")
         character_list.append(enemy)
 
-    # 敵と戦う
+
+def battle(character_list, main_player_name):
     while is_battle_continue(character_list):
         action_list = random.sample(character_list, len(character_list))
         for character in action_list:
             for object in action_list:
-                if character.__class__.__name__ != object.__class__.__name__:
-                    print(f"{character.name}は{object.name}に攻撃した")
+                if character == main_player_name:
+                    print("敵に攻撃しますか？それともHPを回復させますか？")
+                    print("攻撃なら「1」を回復なら「2」を入力してください")
+                    main_player_move = input("あなたの行動を教えてください：")
+                    pass
+                elif character.__class__.__name__ != object.__class__.__name__:
                     character.attack(object)
+                    print(f"{character.name}は{object.name}に攻撃した")
                     break
             print("--------------")
             for character in character_list:
                 print(f"{character.name}:{character.HP}")
             print("--------------\n")
+            if is_battle_continue(character_list) == False:
+                return "終わり"
 
 
 def is_battle_continue(character_list):
